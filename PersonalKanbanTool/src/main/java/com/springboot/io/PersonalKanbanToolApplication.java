2d71f40f2f57ec2463e1d68cc92e9557c3849d67
pchokshi1207 <pchokshi1207@gmail.com>
2019-07-12 17:17:17 -0700

    first Commit

---
+++ PersonalKanbanTool/src/main/java/com/springboot/io/PersonalKanbanToolApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalKanbanToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalKanbanToolApplication.class, args);
+	}
+
+}
