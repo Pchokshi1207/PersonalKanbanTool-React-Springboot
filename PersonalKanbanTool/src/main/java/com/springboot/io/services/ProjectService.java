d01a6d0b4527fca94d934166617f1a099d8d3a3a
pchokshi1207 <pchokshi1207@gmail.com>
2019-07-17 15:29:04 -0700

    Custom Exception for project identifier

--- PersonalKanbanTool/src/main/java/com/springboot/io/services/ProjectService.java
+++ PersonalKanbanTool/src/main/java/com/springboot/io/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.io.domain.Project;
+import com.springboot.io.exceptions.ProjectIdException;
 import com.springboot.io.repositories.ProjectRepository;
 
 @Service
@@ -12,7 +13,13 @@ public class ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch(Exception e) {
+			throw new ProjectIdException("Project Id'"+project.getProjectIdentifier().toUpperCase()+"'already exist");
+		}
+		
 	}
 
 }
--- PersonalKanbanTool/src/main/java/com/springboot/io/web/ProjectController.java
+++ PersonalKanbanTool/src/main/java/com/springboot/io/web/ProjectController.java
@@ -1,17 +1,23 @@
 package com.springboot.io.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.io.domain.Project;
+import com.springboot.io.services.MapValidationErrorService;
 import com.springboot.io.services.ProjectService;
 
 
@@ -22,15 +28,16 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,BindingResult result){
-		if(result.hasErrors()) {
-			
-			return new ResponseEntity<String>( "invalid object request",HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap=mapValidationErrorService.MapValidationService(result);
+		if(errorMap!=null) {return errorMap;}
 		
 		Project project1=projectService.saveOrUpdateProject(project);
-		return new ResponseEntity<Project>(project,HttpStatus.CREATED);
+		return new ResponseEntity<Project>(project1,HttpStatus.CREATED);
 		
 	}
 
